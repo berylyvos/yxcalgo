c0099dfff7d4c09e836590cfd02edbf988fee310
Shingo Gnix <870656738@qq.com>
2023-04-06 22:27:24 +0800

    feat: queue

---
+++ basic/ds/queue/ac154_sliding_window.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+using namespace std;
+// https://www.acwing.com/problem/content/description/156/
+
+const int N = 1000010;
+int a[N], q[N], hh, tt = -1;
+
+int main() {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
+    
+    for (int i = 0; i < n; ++i) {
+        if (hh <= tt && i - q[hh] + 1 > k) ++hh;
+        // 维护队列q单调递增
+        while (hh <= tt && a[i] <= a[q[tt]]) --tt;
+        q[++tt] = i;
+        if (i >= k - 1) printf("%d ", a[q[hh]]);
+    }
+
+    /* deque version
+    for (int i = 0; i < n; ++i) {
+        if (!q.empty() && i - q.front() >= k) q.pop_front();
+        while (!q.empty() && a[i] <= a[q.back()]) q.pop_back();
+        q.push_back(i);
+        if (i >= k - 1) printf("%d ", a[q.front()]);
+    }
+    */
+
+    printf("\n");
+    
+    hh = 0, tt = -1; 
+    for (int i = 0; i < n; ++i) {
+        if (hh <= tt && i - q[hh] + 1 > k) ++hh;
+        // 维护队列q单调递减
+        while (hh <= tt && a[i] >= a[q[tt]]) --tt;
+        q[++tt] = i;
+        if (i >= k - 1) printf("%d ", a[q[hh]]);
+    }
+    
+    return 0;
+}
---
+++ basic/ds/queue/ac829_queue.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// https://www.acwing.com/problem/content/831/
+
+const int N = 100010;
+int q[N], hh, tt = -1;
+
+void push(int x) { q[++tt] = x; }
+
+void pop() { ++hh; }
+
+int top() { return q[hh]; }
+
+bool empty() { return hh > tt; }
+
+int main() {
+    int n, x;
+    cin >> n;
+    string op;
+    while (n--) {
+        cin >> op;
+        if (op == "push") {
+            cin >> x;
+            push(x);
+        } else if (op == "query") {
+            cout << top() << '\n';
+        } else if (op == "pop") {
+            pop();
+        } else {
+            cout << (empty() ? "YES" : "NO") << endl;
+        }
+    }
+    
+    return 0;
+}
