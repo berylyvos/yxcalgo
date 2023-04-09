2986e884129b901b5c41d53dc4f9deb154ad6801
Shingo Gnix <870656738@qq.com>
2023-04-09 21:08:21 +0800

    feat: trie

---
+++ basic/ds/trie/ac143_max_xor_pair.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+// https://www.acwing.com/problem/content/description/145/
+
+const int N = 100010;
+int son[N * 31][2], idx;
+int a[N], n;
+
+void insert(int x) {
+    int p = 0;
+    for (int i = 30; ~i; --i) {
+        int &s = son[p][x >> i & 1];
+        if (!s) s = ++idx;
+        p = s;
+    }
+}
+
+int query(int x) {
+    int p = 0, res = 0;
+    for (int i = 30; ~i; --i) {
+        int u = (x >> i & 1);
+        if (son[p][!u]) {
+            res += (1 << i);
+            p = son[p][!u];
+        } else {
+            p = son[p][u];
+        }
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        insert(a[i]);
+    }
+    
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        res = max(res, query(a[i]));
+    }
+    cout << res;
+    
+    return 0;
+}
---
+++ basic/ds/trie/ac835_trie.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+// https://www.acwing.com/problem/content/description/837/
+
+const int N = 100010;
+int son[N][26], cnt[N];
+int idx, m;
+
+void insert(const string& x) {
+    int p = 0;
+    for (int i = 0, n = x.size(); i < n; ++i) {
+        int &s = son[p][x[i] - 'a'];
+        if (!s) s = ++idx;
+        p = s;
+    }
+    cnt[p]++;
+}
+
+int query(const string& x) {
+    int p = 0;
+    for (int i = 0, n = x.size(); i < n; ++i) {
+        int s = son[p][x[i] - 'a'];
+        if (!s) return 0;
+        p = s;
+    }
+    return cnt[p];
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> m;
+    
+    char op;
+    string x;
+    while (m--) {
+        cin >> op >> x;
+        if (op == 'I') insert(x);
+        else cout << query(x) << '\n';
+    }
+    
+    return 0;
+}
